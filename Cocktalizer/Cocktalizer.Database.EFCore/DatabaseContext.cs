47dd85ca6c87abd716d0ecedae46c9bba1148854
d4n0n_myself <danon.sibaev@gmail.com>
2019-07-04 22:00:11 +0300

    DbContext

--- Cocktalizer/Cocktalizer.Database.EFCore/DatabaseContext.cs
+++ Cocktalizer/Cocktalizer.Database.EFCore/DatabaseContext.cs
@@ -1,3 +1,4 @@
+using Cocktalizer.Configuration;
 using Cocktalizer.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,18 @@ namespace Cocktalizer.Database.EFCore
 		public DbSet<Cocktail> Cocktails { get; set; }
 		
 		public DbSet<Ingridient> Ingridients { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(ApplicationConfiguration.GetConnectionString());
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Cocktail>().HasIndex(x => x.Name);
+			modelBuilder.Entity<Cocktail>().HasMany(x => x.Decorations).WithOne(x => x.Cocktail);
+			base.OnModelCreating(modelBuilder);
+		}
 	}
 }
