8b0a02ae0d932e44cca80f589e74e8c26213e34b
d4n0n_myself <danon.sibaev@gmail.com>
2019-07-18 21:25:07 +0300

    добавил логирование

--- Cocktalizer/Cocktalizer.Configuration/ApplicationConfiguration.cs
+++ Cocktalizer/Cocktalizer.Configuration/ApplicationConfiguration.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using Microsoft.Extensions.Configuration;
 using NLog;
+using NLog.Config;
 
 namespace Cocktalizer.Configuration
 {
@@ -10,7 +13,17 @@ namespace Cocktalizer.Configuration
 
 		public static IConfiguration Database { get; } = Configuration.GetSection("Database");
 
-		public static ILogger Logger => LogManager.GetLogger("Default");
+		public static ILogger Logger
+		{
+			get
+			{
+				var nlogConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nlog.config");
+				var configuration = new XmlLoggingConfiguration(nlogConfigPath);
+				var loggerFactory = new LogFactory(configuration);
+				var logger = loggerFactory.GetLogger("Default");
+				return logger;
+			}
+		}
 
 		public static string GetConnectionString() => Database["ConnectionString"];
 	}
